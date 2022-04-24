57e7b8ed42e01396c6f1bec36f94bb39fcb5a5e4
coco-egaliciar <esteban@coconutt.io>
2022-04-24 18:09:08 -0500

    feat: Render principal robot

--- src/js/animation3D.js
+++ src/js/animation3D.js
@@ -6,120 +6,77 @@ import fbxFile from '../resource/ParaWeb.fbx'
 
 export class Animate3D {
   constructor () {
-    this.canvas = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true
-    })
-    this.canvas.shadowMap.enabled = true
-    this.canvas.shadowMap.type = THREE.PCFSoftShadowMap
-    this.canvas.physicallyCorrectLights = true
-    this.canvas.toneMapping = THREE.ACESFilmicToneMapping
-    this.canvas.outputEncoding = THREE.sRGBEncoding
+    const scene = new THREE.Scene()
+    // scene.add(new THREE.AxesHelper(50))
 
-    const modelDiv = document.getElementById('model')
-    modelDiv.appendChild(this.canvas.domElement)
+    const light = new THREE.PointLight()
+    light.position.set(0.8, 1.4, 1.0)
+    scene.add(light)
 
-    this.canvas.setSize(modelDiv.offsetWidth, modelDiv.offsetHeight)
+    const ambientLight = new THREE.AmbientLight()
+    scene.add(ambientLight)
 
-    window.addEventListener('resize', () => { this.OnWindowResize() }, false)
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    camera.position.set(0.8, 1.4, 1.0)
 
-    const fov = 60
-    const aspect = window.innerHeight / window.innerWidth
-    const near = 5
-    const far = 5000.0
-    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-    this._camera.position.set(150 / 5, 150 / 5, 200 / 5)
+    const renderer = new THREE.WebGLRenderer({ alpha: true })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    const model = document.querySelector('#model')
+    model.appendChild(renderer.domElement)
 
-    this.scene = new THREE.Scene()
+    const controls = new OrbitControls(camera, model)
+    // controls.enableDamping = true
+    controls.target.set(0, 1, 0)
+    controls.enabled = false
 
-    let light = new THREE.DirectionalLight(0xFFFFFF)
-    light.position.set(20, 100, 10)
-    light.target.position.set(0, 0, 0)
-    light.castShadow = true
-    light.shadow.bias = -0.001
-    light.shadow.mapSize.width = 2048
-    light.shadow.mapSize.height = 2048
-    light.shadow.camera.near = 0.1
-    light.shadow.camera.far = 500.0
-    light.shadow.camera.near = 0.5
-    light.shadow.camera.far = 500.0
-    light.shadow.camera.left = 100
-    light.shadow.camera.right = -100
-    light.shadow.camera.top = 100
-    light.shadow.camera.bottom = -100
-    this.scene.add(light)
+    const fbxLoader = new FBXLoader()
+    fbxLoader.load(
+      fbxFile,
+      (object) => {
+        object.traverse(function (child) {
 
-    light = new THREE.AmbientLight(0xFFFFFF)
-    this.scene.add(light)
+        })
+        object.scale.set(0.01 * 1.1, 0.01 * 1.1, 0.01 * 1.1)
 
-    this._controls = new OrbitControls(this._camera, this.canvas.domElement)
-    this._controls.enabled = false
-    this._controls.target.set(0, 10, 0)
-    this._controls.update()
+        scene.add(object)
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
 
-    this.LoadAnimatedModelAndPlay(fbxFile, new THREE.Vector3(0, 0, 0))
-    // 'https://threejs.org/examples/models/fbx/Samba%20Dancing.fbx', new THREE.Vector3(0, 0, 0)
+    window.addEventListener('resize', onWindowResize, false)
 
+    function onWindowResize () {
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      render()
+    }
 
-    this._mixers = []
-    this._previousRAF = null
+    function animate () {
+      requestAnimationFrame(animate)
+      controls.update()
+      render()
+    }
 
-    this._scrollAmount = 0.0
-    this.RAF()
-  }
+    function render () {
+      renderer.render(scene, camera)
+    }
 
-  LoadAnimatedModelAndPlay (modelFile, offset) {
-    const loader = new FBXLoader()
-    // loader.setPath(path)
-    loader.load(modelFile, (fbx) => {
-      fbx.scale.setScalar(0.2)
-      fbx.traverse(c => {
-        c.castShadow = true
-      })
-      fbx.position.copy(offset)
-
-      const anim = new FBXLoader()
-      // anim.setPath(path)
-      anim.load(modelFile, (anim) => {
-        const m = new THREE.AnimationMixer(fbx)
-        this._mixers.push(m)
-        const idle = m.clipAction(anim.animations[0])
-        idle.play()
-      })
-      this.scene.add(fbx)
-    })
+    this.scene = scene
+    animate()
   }
 
   OnScroll (position) {
-    const amount = Math.min(0.174533)
-    this.scene.rotateY(amount)
-    this._controls.update()
-  }
-
-  OnWindowResize () {
-    this._camera.aspect = window.innerWidth / window.innerHeight
-    this._camera.updateProjectionMatrix()
-    this.canvas.setSize(window.innerWidth, window.innerHeight)
-  }
-
-  Step (timeElapsed) {
-    const timeElapsedS = timeElapsed * 0.001
-    if (this._mixers) {
-      this._mixers.map(m => m.update(timeElapsedS))
-    }
-  }
-
-  RAF () {
-    requestAnimationFrame((t) => {
-      if (this._previousRAF === null) {
-        this._previousRAF = t
-      }
-
-      this.RAF()
-
-      this.canvas.render(this.scene, this._camera)
-      this.Step(t - this._previousRAF)
-      this._previousRAF = t
-    })
+    this.scene.rotateY(Math.PI / 30)
   }
 }
